9138771fd2667efd51754492fab39ec6f212e3e0
inclee <invictuslee@163.com>
2018-01-25 11:04:34 +0800

    update BlockingQueue

--- Queue/Cxx/BlockingQueue/BlockingQueue.h
+++ Queue/Cxx/BlockingQueue/BlockingQueue.h
@@ -5,6 +5,7 @@
  *      Author: lixu
  */
 
+
 #ifndef SRC_BLOCKINGQUEUE_H_
 #define SRC_BLOCKINGQUEUE_H_
 
@@ -15,16 +16,38 @@
 template<typename T>
 class BlockingQueue {
 public:
-	BlockingQueue(unsigned int capacity);
-	void push(const T& t);
-	T& pop();
-	virtual ~BlockingQueue();
+    BlockingQueue(size_t capacity):
+    _queue(),
+    _mtx(),
+    _cond(),
+    _capacity(capacity){
+    }
+    void push(const T& t){
+        if(_queue.size() < _capacity){
+            _queue.push(t);
+            _cond.notify_all();
+        }else {
+            std::unique_lock<std::mutex> lock(_mtx);
+            _cond.wait(lock);
+        }
+    }
+    T& pop(){
+        if(_queue.size() == 0){
+            std::unique_lock<std::mutex> lock(_mtx);
+            _cond.wait(lock);
+        }
+        T& t = _queue.front();
+        _queue.pop();
+        _cond.notify_all();
+        return t;
+    }
+    size_t size (){ return _queue.size();}
+    virtual ~BlockingQueue(){};
 private:
 	std::queue<T> _queue;
 	std::mutex _mtx;
 	std::condition_variable _cond;
-	std::unique_lock<std::mutex>& _lock;
-	unsigned int _capacity;
+	size_t _capacity;
 };
 
 #endif /* SRC_BLOCKINGQUEUE_H_ */
